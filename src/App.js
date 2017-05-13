31c48c613b46c01c72075735f632aed6c153f6a2
Li <li3zhang@gmail.com>
2017-05-13 17:50:35 -0400

    merge post with master

--- src/App.js
+++ src/App.js
@@ -39,9 +39,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
           <form>
             <input id="date" type="date" onChange={this.update('dateTime')}></input>
             <input onChange={this.update('userNumber')}></input>
