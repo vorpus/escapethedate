7549bb943aaaed068e47033907364608c99c833e
kr3110 <n.nkr3110@gmail.com>
2017-05-14 02:06:16 -0400

    friend call

--- server/disrupt/app.js
+++ server/disrupt/app.js
@@ -1,17 +1,15 @@
 //injections
 var express = require('express');
-
 var https = require('https');
 var morgan = require('morgan');
 var nexmo = require('nexmo');
 var alarm = require('alarm');
 var phone = '12035338282';
 var bodyParser = require('body-parser');
-var signup=require('./user');
+var signup = require('./user');
 var app = express();
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
-
 //mongo injection and connection
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/disrupt');
@@ -20,27 +18,20 @@ var message = 'is everything ok?';
 var privateKeyFile = './private.key';
 //logger console
 app.use(morgan('dev'));
-
 //middleware cors
 app.use(function (req, res, next) {
-
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
-
     // Pass to next layer of middleware
     next();
 });
-
 //sms setup
 const privateKey = require('fs').readFileSync(privateKeyFile);
 var Nex = new nexmo({
@@ -50,15 +41,11 @@ var Nex = new nexmo({
     , privateKey: privateKey
 });
 //alarm
-app.post('/inbound', function (req, res) {
-    recietex(req.body, res);
-});
 var apiRoutes = express.Router();
 app.use('/api', apiRoutes);
-
 apiRoutes.post('/signup', function (req, res) {
     console.log(req.body.user)
-     if (!req.body.user) {
+    if (!req.body.user) {
         res.json({
             success: false
             , msg: 'Please pass user fields'
@@ -87,72 +74,101 @@ apiRoutes.post('/signup', function (req, res) {
         });
     }
 });
-
 // const THIRTY_MINUTES_IN_MILLISECONDS = 30 * 60 * 1000;
 const THIRTY_MINUTES_IN_MILLISECONDS = 30 * 1000;
 
 function detailsObjectWithAddedMinutes(details, minutesToAdd) {
-  const addedMinutes = minutesToAdd * 60 * 1000;
-
-  const newTime = new Date(+ new Date() + addedMinutes);
-  return Object.assign({}, details, {datetime: newTime});
+    const addedMinutes = minutesToAdd * 60 * 1000;
+    const newTime = new Date(+new Date() + addedMinutes);
+    return Object.assign({}, details, {
+        datetime: newTime
+    });
 }
 
 function scheduleMessages(details) {
-  const {user, datetime, datee, friend} = details;
-  sendtxt(`1${user}`, `Hey! We\'re all set for your date at ${datetime}, text STOP at any time to disable these reminders!`);
-  alarm(new Date(datetime), function() {
-    console.log(`sending message to 1${user}`);
-    sendtxt(`1${user}`, 'Hey! Everything ok?\n(O)K - Text again in 30!\n(S)TOP - Everything is peachy!\n(E)SCAPE - Call me!\n(H)ELP - Call my SOS!');
-
-    const newAlert = detailsObjectWithAddedMinutes(details, 30);
-    scheduleNoResponseReceived(newAlert);
-  });
+    const {
+        user, datetime, datee, friend
+    } = details;
+    sendtxt(`1${user}`, `Hey! We\'re all set for your date at ${datetime}, text STOP at any time to disable these reminders!`);
+    alarm(new Date(datetime), function () {
+        console.log(`sending message to 1${user}`);
+        sendtxt(`1${user}`, 'Hey! Everything ok?\n(O)K - Text again in 30!\n(S)TOP - Everything is peachy!\n(E)SCAPE - Call me!\n(H)ELP - Call my SOS!');
+        const newAlert = detailsObjectWithAddedMinutes(details, 30);
+        scheduleNoResponseReceived(newAlert);
+    });
 }
 
 function scheduleNoResponseReceived(details) {
-    const {user, datetime, datee, friend} = details;
+    const {
+        user, datetime, datee, friend
+    } = details;
     console.log(`Notifying again at ${datetime}`);
     alarm(new Date(datetime), () => {
-      console.log(`it's been 30 mins since 1${user}`)
-      sendtxt(`1${user}`, 'Checking in again, we\'ll notify your friend soon! Reply STOP to disable!')
+        console.log(`it's been 30 mins since 1${user}`)
+        sendtxt(`1${user}`, 'Checking in again, we\'ll notify your friend soon! Reply STOP to disable!')
     });
 }
+app.post('/inbound', function (req, res) {
+    recietex(req.body, res);
+});
 
 function recietex(params, res) {
     if (!params.to || !params.msisdn) {
         console.log('This is not a valid inbound SMS message!');
     }
     else {
-        console.log('Success');
-        console.log(params.text);
-    }
-    res.status(200).end();
-};
-app.get('/', function (req, res) {
-    sendtxt('19294351864', 'hi');
-});
-var call = function (reciever) {
-    Nex.calls.create({
-        to: [{
-            type: 'phone'
-            , number: reciever
+        var text = params.text;
+        var sender = params.msisdn;
+        if (text.toLowerCase() == 'e' || text.toLowerCase() == 'escape') {
+            call(sender);
+        }
+        else if (text.toLowerCase() == 'h' || text.toLowerCase() == 'help') {
+            //call friend
+            signup.findOne({
+                user: sender
+            }, function (err, signup) {
+                if (err) throw err;
+                if (!signup) {
+                    return res.status(403).send({
+                        success: false
+                        , msg: 'number not found.'
+                    });
+                }
+                else {
+                    call(signup.friend);
+                }
+            });
+        }
+            else if (text.toLowerCase() == 'ok' || text.toLowerCase() == 'o') {
+                //
+            }
+            else if (text.toLowerCase() == 's' || text.toLowerCase() == 'stop') {
+                //
+            }
+        }
+        res.status(200).end();
+    };
+    var call = function (reciever) {
+        Nex.calls.create({
+            to: [{
+                type: 'phone'
+                , number: reciever
   }]
-        , from: {
-            type: 'phone'
-            , number: phone // your virtual number
-        }
-        , answer_url: ['https://nexmo-community.github.io/ncco-examples/first_call_talk.json']
-    }, function (err, res) {
-        if (err) {
-            console.error(err);
-        }
-        else {
-            console.log(res);
-        }
-    });
-};
-var sendtxt = function (reciever, message) {
-    Nex.message.sendSms(phone, reciever, message);
-};
-app.listen('3030');
+            , from: {
+                type: 'phone'
+                , number: phone // your virtual number
+            }
+            , answer_url: ['https://nexmo-community.github.io/ncco-examples/first_call_talk.json']
+        }, function (err, res) {
+            if (err) {
+                console.error(err);
+            }
+            else {
+                console.log(res);
+            }
+        });
+    };
+    var sendtxt = function (reciever, message) {
+        Nex.message.sendSms(phone, reciever, message);
+    };
+    app.listen('3030');
